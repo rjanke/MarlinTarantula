69ced09b33216376e8575230e50985a3df928148
rjanke <jankersr@yahoo.com>
2018-03-08 16:35:06 -0700

    Changed PID settings for Hotend and Bed.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -141,18 +141,18 @@
 /**
  * TEVO Tarantula Custom PID Settings - Stock Hotend
  */
-#define  hot_Kp 9.84
-#define  hot_Ki 0.50
-#define  hot_Kd 48.17
+#define  hot_Kp 16.54
+#define  hot_Ki 1.40
+#define  hot_Kd 48.78
 // FIND YOUR OWN: "M303 E0 C8 S200" to run autotune on the hotend at 200 degreesC for 8 cycles.
 // More info here: http://reprap.org/wiki/PID_Tuning
 
 /**
  * TEVO Tarantula Custom PID Settings - Stock Heatbed
  */
-#define  bed_Kp 984.88
-#define  bed_Ki 193.91
-#define  bed_Kd 1250.55
+#define  bed_Kp 247.98
+#define  bed_Ki 48.05
+#define  bed_Kd 319.92
 // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 // More info here: http://reprap.org/wiki/PID_Tuning
 
